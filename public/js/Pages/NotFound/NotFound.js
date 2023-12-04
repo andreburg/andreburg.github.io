35bfbd323945d53c178e5275aaf604f6cd18d9d3
andre <andreludovicusburger@gmail.com>
2023-12-04 09:44:14 +0200

    update not found page

--- public/js/Pages/NotFound/NotFound.js
+++ public/js/Pages/NotFound/NotFound.js
@@ -1,8 +1,18 @@
 import Page from "../Page.js";
+import State from "../../State/State.js";
 
 export default class NotFoundPage extends Page {
     constructor(params) {
         super(params);
+
+        this.globalState = new State({});
+        this.globalState.notifyChange({
+            ...this.globalState.state,
+            page: {
+                ...this.globalState.state.page,
+                route: "/"
+            }
+        });
     };
 
     getHtml() {
